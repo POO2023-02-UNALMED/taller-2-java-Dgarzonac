0a4023ac742dae887ea54d1ebdab0f78ab91b619
Dgarzonac <123409139+Dgarzonac@users.noreply.github.com>
2023-08-17 18:23:16 -0500

    correcciones

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -2,11 +2,13 @@ package test;
 
 public class Asiento {
 	int color;
-	int precio
+	int precio;
 	int registro;
 	
-	void cambiarColor(color: String) {
-		if(color == "Rojo" || color = "Blanco" || color = "Verde" || color = "Amarillo" || color="negro" ||)
+	void cambiarColor(String color) {
+		if(color == "Rojo" || color = "Blanco" || color = "Verde" || color = "Amarillo" || color="Negro" ||)
+		{
 			this.color = color
+		}
 	}
 }
--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,19 +1,19 @@
 package test;
 
 public class Motor {
-	int numeroCilindors;
+	int numeroCilindros;
 	int registro;
 	String tipo;
 	
 	void cambiarRegistro(int registro) 
 	{
-		this.registro = registro
+		this.registro = registro;
 	}
 	void asignarTipo(String tipo)
 	{
 		if(tipo=="electrico" || tipo=="gasolina")
 		{
-			this.tipo = tipo
+			this.tipo = tipo;
 		}
 	}
 }
